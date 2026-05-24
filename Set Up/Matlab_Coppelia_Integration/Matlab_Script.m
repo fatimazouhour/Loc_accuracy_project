d9e5a9cc7a8fea7c5b81baccbb28485e1390ff1b
hussein-ghaddar <husainghaddar5@gmail.com>
2026-05-24 21:15:59 +0000

    Update Matlab_Script.m

--- Set Up/Matlab_Coppelia_Integration/Matlab_Script.m
+++ Set Up/Matlab_Coppelia_Integration/Matlab_Script.m
@@ -1,8 +1,7 @@
 clc;
 clear;
 
-%adding the matlab file from zmqRemoteApi from the programming file in CoppeliaSim_Edu_V4_10_0_rev0_Ubuntu22_04
-addpath('/home/husain5/Downloads/CoppeliaSim_Edu_V4_10_0_rev0_Ubuntu22_04/programming/zmqRemoteApi/clients/matlab');
+addpath('/home/husain5/Documents/CoppeliaSim/CoppeliaSim_Edu_V4_10_0_rev0_Ubuntu22_04/programming/zmqRemoteApi/clients/matlab');
 rehash toolboxcache;
 
 client = RemoteAPIClient();
@@ -11,8 +10,13 @@ sim = client.require('sim');
 robot = sim.getObject('/PioneerP3DX');
 leftMotor = sim.getObject('/PioneerP3DX/leftMotor');
 rightMotor = sim.getObject('/PioneerP3DX/rightMotor');
-simulationiteration = 1000
+simulationiteration = 50
 sim.startSimulation();
+noise_accel= 0.003; % consistent with Mpu-9150 data sheet mentioned in the paper
+noise_gyro= 0.005; % ...  
+noise_odo_x = 0.2;   % the paper reflects the REAL pioneer 
+noise_odo_y= 0.02;
+noise_gps = 0.3161; %consistent with  uBlox NEO-6m datasheet
 
 %This for loops runs infinitly until we stop the matlab execution manually
 %to stop printing data and from the simulation to make the robot stop
@@ -20,23 +24,23 @@ sim.startSimulation();
 while sim.getSimulationTime() < simulationiteration
 
     % Getting the Position using GPS from CoppeliaSim
-    gpsX = sim.getFloatSignal('gpsX');
-    gpsY = sim.getFloatSignal('gpsY');
-    gpsZ = sim.getFloatSignal('gpsZ');
+    gpsX = sim.getFloatSignal('gpsX') + noise_gps*randn();
+    gpsY = sim.getFloatSignal('gpsY') + noise_gps*randn();
+    gpsZ = sim.getFloatSignal('gpsZ') + noise_gps*randn();
     
     gps = [gpsX gpsY gpsZ];
 
     %Getting the angles using Gyroscope from CoppeliaSim 
-    roll = sim.getFloatSignal('roll');
-    pitch = sim.getFloatSignal('pitch');
-    yaw = sim.getFloatSignal('yaw');
+    roll = sim.getFloatSignal('roll') + noise_gyro*randn();
+    pitch = sim.getFloatSignal('pitch') + noise_gyro*randn();
+    yaw = sim.getFloatSignal('yaw') + noise_gyro*randn();
 
     angle = [roll,pitch,yaw];
 
     % Getting the acceleration using the Accelerometer from CoppeliaSim
-    accelX = sim.getFloatSignal('accelX');
-    accelY = sim.getFloatSignal('accelY');
-    accelZ = sim.getFloatSignal('accelZ');
+    accelX = sim.getFloatSignal('accelX') + noise_accel*randn();
+    accelY = sim.getFloatSignal('accelY') + noise_accel*randn();
+    accelZ = sim.getFloatSignal('accelZ') + noise_accel*randn();
 
     % Getting the left and right encoder using leftmotor and rightmore from Coppeliasim 
     leftEncoder = sim.getJointPosition(leftMotor);
@@ -44,7 +48,7 @@ while sim.getSimulationTime() < simulationiteration
 
     fprintf('x=%.2f y=%.2f yaw=%.2f leftEnc=%.2f rightEnc=%.2f\n', ...
         gpsX, gpsY, yaw, leftEncoder, rightEncoder);
-
+    
     % Example command for the robot to move
     sim.setJointTargetVelocity(leftMotor, 1.0);
     sim.setJointTargetVelocity(rightMotor, 2.0);
