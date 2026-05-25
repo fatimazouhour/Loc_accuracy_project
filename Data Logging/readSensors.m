e1e90c99b3ddb66e0fb7c95d36d6431560573b49
fatimazouhour <thefatimazouhour@gmail.com>
2026-05-25 22:01:24 +0200

    update change

--- Data Logging/readSensors.m
+++ Data Logging/readSensors.m
@@ -10,27 +10,33 @@ N = numel(gt.t);
 % for the accelerometer 
 accelB = raw.accel;
 % we flip the sign because in the sensor it is -9.81
-accelB(:,3)=-accel(:,3);
+accelB(:,3)=-accelB(:,3);
 
 
 % add gaussian noise with a zero mean value
 accelB(:,1) = accelB(:,1) + parameters.noise_accel * randn(N,1);
 accelB(:,2) = accelB(:,2) +p.noise_accel * randn(N,1);
 accelB(:,3) = accelB(:,3)  + p.noise_accel * randn(N,1);
- 
-gyroB        = gyroStream;
-gyroB(:,3)   = gyroB(:,3) + p.bias.gyroZ + p.noise.gyro * randn(N,1);
-gyroB(:,1:2) = gyroB(:,1:2)              + p.noise.gyro * randn(N,2);
+
+%
+gyroB= raw.gyro;  % bas 3mlna copy to avaod changing raw directly
+
+
+% add noise
+gyroB(:, 1) = gyroB(:, 1) + parameters.noise_gyro * randn(N, 1);
+gyroB(:, 2) = gyroB(:, 2) + parameters.noise_gyro * randn(N, 1);
+gyroB(:, 3) = gyroB(:, 3) + parameters.noise_gyro * randn(N, 1);
  
 
-r  = p.robot.wheelRadius;
-vx = r * (gt.wheelOmega(:,2) + gt.wheelOmega(:,1)) / 2;
-vy = zeros(N,1);
-odoVelB = [vx + p.noise.odoVel*randn(N,1), ...
-           vy + p.noise.odoVel*randn(N,1)];
+% 
+r= parameters.wheelRadius;
+vx_odo =r*(gt.wheelOmega(:,2)+ gt.wheelOmega(:,1))/2;
+vy = zeros(N,1); % from professors slides for differential drive robots
+ odoVelB = [ vx_odo + parameters.noise_odo_x * randn(N, 1), vy_odo + parameters.noise_odo_y * randn(N, 1) ];
  
-gpsW = gt.posW(:,1:2) + p.noise.gpsPos * randn(N,2);
- 
+
+ % NOTE: gps already in 
+gpsW = raw.gps(:,1:2) + parameters.noise_gps * randn(N, 2); 
 sensors.t       = gt.t;
 sensors.accelB  = accelB;
 sensors.gyroB   = gyroB;
