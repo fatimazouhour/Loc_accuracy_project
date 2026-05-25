81a13642b6d2540e9a974ab461881cff2e9f9dd2
fatimazouhour <thefatimazouhour@gmail.com>
2026-05-25 02:16:53 +0200

    add parameter

--- Data Logging/config.m
+++ Data Logging/config.m
@@ -9,7 +9,7 @@ dt =  0.05; % sampling time is 1s & CoppeliaSim's default physics dt is 50 ms; m
 ipAddress = '127.0.0.1' ; % local host
 port= 19999; % default Api
 
-%% from manufacturers data sheet 
+%% from manufacturers data cheat 
 wheelRadius= 0.0975; %m
 wheelBase = 0.381 ; % dist between both wheels
 
@@ -20,9 +20,9 @@ speed = 0.2;
 
 %% artificial sensor noise
 
-noise_accel= 0.003; % consistent with Mpu-9150 data sheet mentioned in the paper
+noise_accel= 0.003; % consistent with Mpu-9150 datacheat mentioned in the paper
 noise_gyro= 0.005; % ...  
-noise_odo_x = 0.2;   % the paper reflects the REAL pioneer 
+noise_odo_x = 0.02;   % the paper reflects the REAL pioneer 
 noise_odo_y= 0.02;
 noise_gps = 0.3161; %consistent with  uBlox NEO-6m datasheet
 
@@ -33,3 +33,22 @@ g= 9.81;
 %% output data collection 
 dataDir= './data';
 dataFile= 'sensor.mat';
+
+%% usingg artificial sensor file
+%
+
+%these are to be used for the functions later on ,
+% we pack every thing as one struct so it can be passed as a sigle argument
+% later on 
+parameters.dt= dt;
+parameters.T            = T;
+parameters.g            = g;
+parameters.wheelRadius  = wheelRadius;
+parameters.wheelBase    = wheelBase;
+parameters.noise_accel  = noise_accel;
+parameters.noise_gyro   = noise_gyro;
+parameters.noise_gps    = noise_gps;
+parameters.noise_odo_x  = noise_odo_x;
+parameters.noise_odo_y  = noise_odo_y;
+parameters.dataDir      = dataDir;
+parameters.dataFile     = dataFile;
