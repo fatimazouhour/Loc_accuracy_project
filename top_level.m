1ee1611090b40f803b23c4d006f9a98661347677
MayaKawssan <130219380+MayaKawssan@users.noreply.github.com>
2026-06-20 21:16:08 +0200

    Update top_level.m

--- top_level.m
+++ top_level.m
@@ -12,8 +12,19 @@ cd(here);
  run('BaseMatlab');     
 run('ekf_gps_ins');      
 run('ekf_gps_odo');      
-run('ekf_fusion');    
-Y_train = [x_fused(:), y_fused(:)];
+run('ekf_fusion'); 
+
+FORCE_RETRAIN = false; 
+
+if FORCE_RETRAIN || ~exist('trainedANN.mat', 'file')
+    fprintf('Training model because it is missing or forced...\n');
+    % This calls your new training/validation file
+    run('ann_training'); 
+else
+    fprintf('Loading existing trained model...\n');
+    % This loads the model object directly into your workspace
+    load('trainedANN.mat', 'ann');
+end
 
 % run('ekf_ANN_fusion'); 
 
