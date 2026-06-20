1d7e1c88269dd49408ece29ea136395ad20b003f
MayaKawssan <130219380+MayaKawssan@users.noreply.github.com>
2026-06-21 01:22:44 +0200

    Update ekf_ANN_fusion.m

--- KALMAN/ekf_ANN_fusion.m
+++ KALMAN/ekf_ANN_fusion.m
@@ -1,8 +1,4 @@
-clear 
-clc
 
-addpath('C:\Users\Fatima\Desktop\Mechatronics\Robotics\Robotics_Project\KALMAN');
-addpath('C:\Users\Fatima\Desktop\Mechatronics\Robotics\Robotics_Project\ANN');
 %load data
 Base = load('kf_input.mat');
 K2   = load('kf2_output.mat');  kf2 = K2.kf2;
@@ -18,37 +14,7 @@ if all(gps_available)
              'Enable an outage in the EKF scripts so the mask propagates here.']);
 end
 
-%% ann inputs 
 
-accel   = [Base.accelX_hist(:), Base.accelY_hist(:), zeros(N,1)]; % [ax ay az]
-velINS  = kf2.vel.';                       % KF-2 world velocity [vx vy]
-heading = kf2.theta(:);                    % KF-2 heading [rad]
-odoVel  = [Base.odom_v_hist(:), zeros(N,1)];   % odometer body velocity [vx vy]
-
-% ANN feature matrix eq 3 in paper
-
-acc_mag     = sqrt(sum(accel.^2, 2));
-cum_acc_mag = cumsum(acc_mag) * dt;
-vel_mag     = sqrt(sum(velINS.^2, 2));
-cum_vel_mag = cumsum(vel_mag) * dt;
-dpsi        = [0; diff(unwrap(heading))];  % per-step heading change
-cum_heading = cumsum(dpsi);                % total accumulated turn [rad]
-vx_odo      = odoVel(:,1);   cum_vx_odo = cumsum(vx_odo) * dt;
-vy_odo      = odoVel(:,2);   cum_vy_odo = cumsum(vy_odo) * dt;
-
-X = [acc_mag, cum_acc_mag, vel_mag, cum_vel_mag, heading, ...
-     cum_heading, vx_odo, cum_vx_odo, vy_odo, cum_vy_odo];   % N x 10
-
-%wanted position
-Y = kf2.pos.';                             % N x 2
-
-%train ann on gps data only 
-tr_idx = find(gps_available);
-if numel(tr_idx) < 20
-    error('Too few GPS-available samples to train the ANN.');
-end
-ann = RobotANN_Class(size(X,2));
-ann.trainANN(X(tr_idx,:), Y(tr_idx,:));
 
 %ann prediction
 pos_ann = ann.predict(X).';                % 2 x N
