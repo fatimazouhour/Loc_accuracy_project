4c267a6e2bc3b8d79375ebfa86c2a55522a2dc93
MayaKawssan <130219380+MayaKawssan@users.noreply.github.com>
2026-06-21 01:23:29 +0200

    Update ekf_ANN_fusion.m

--- KALMAN/ekf_ANN_fusion.m
+++ KALMAN/ekf_ANN_fusion.m
@@ -44,14 +44,3 @@ fprintf('-- whole run --\n');
 fprintf('KF-2     : %.4f\n', rmse(truth-kf2.pos));
 fprintf('Fusion B : %.4f\n', rmse(truth-final));
 
-%% plots
-figure; hold on; grid on; axis equal
-plot(truth(1,:),  truth(2,:),  'g-',  'LineWidth',2,   'DisplayName','Truth')
-plot(kf2.pos(1,:),kf2.pos(2,:),'b-',  'LineWidth',1,   'DisplayName','KF-2')
-plot(pos_ann(1,:),pos_ann(2,:),'-',   'Color',[.85 .33 .1],'DisplayName','ANN')
-plot(final(1,:),  final(2,:),  'k--', 'LineWidth',1.8, 'DisplayName','Fusion B (ANN+KF-2)')
-if any(out)
-    plot(truth(1,out),truth(2,out),'rx','MarkerSize',5,'DisplayName','outage truth')
-end
-legend('Location','best'); xlabel('x (m)'); ylabel('y (m)')
-title('Fusion B (paper Eq. 5): ANN + KF-2 during GPS outage')
